e5905c5782c1b868fbe36f6dad06a708b445535b
Ning Wang <ning.wang@utu.fi>
2020-12-08 11:54:30 +0200

    Upload New File

---
+++ mut_ref_var.py
@@ -0,0 +1,146 @@
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+import read_reference
+
+def mut_ref(df,loc,ref_file,complement,transcript,translate,table_def):
+    chromosome=loc.split(":")[0]
+    ref=read_reference.read_reference_sequence(ref_file,chromosome)
+    coordinates=loc.split(":")[1]
+    start=int(coordinates.split("-")[0])-1 # python 0-based
+    end=int(coordinates.split("-")[1])-1 
+
+    line3=""
+    for i in range(0,df.shape[0]):
+        if i==0:
+            if start<int(df.loc[i,"Position"])-1: # python 0-based
+                mut_seq=ref[start:int(df.loc[i,"Position"])-1] # python last index not include
+            elif start==int(df.loc[i,"Position"])-1:
+                mut_seq=""
+            mut_seq_T=df.loc[i,"ALT"]
+            mut_seq=mut_seq+mut_seq_T
+            line3=line3+df.loc[i,"Chromosome"]+"_"+df.loc[i,"Position"]+"_"+df.loc[i,"REF"]+"_"+df.loc[i,"ALT"]+"_"+df.loc[i,"Genotype"]+" / "
+            start_Next = int(df.loc[i,"Position"])-1+len(df.loc[i,"REF"])
+            if (i+1)==df.shape[0]:
+                if start_Next>end:
+                    end = start_Next
+                elif start_Next==end:
+                    mut_seq_T2=ref[start_Next]
+                    mut_seq=mut_seq+mut_seq_T2
+                elif start_Next<end:
+                    mut_seq_T2=ref[start_Next:end+1]
+                    mut_seq=mut_seq+mut_seq_T2
+                    mut_seq=mut_seq.upper()
+
+        else:
+            # SNV, MNV, indels happen to have same form
+            mut_seq_T2=ref[start_Next:int(df.loc[i,"Position"])-1]
+            mut_seq=mut_seq+mut_seq_T2          
+            
+            mut_seq_T=df.loc[i,"ALT"]
+            mut_seq=mut_seq+mut_seq_T
+            line3=line3+df.loc[i,"Chromosome"]+"_"+df.loc[i,"Position"]+"_"+df.loc[i,"REF"]+"_"+df.loc[i,"ALT"]+"_"+df.loc[i,"Genotype"]+" / "          
+            start_Next = int(df.loc[i,"Position"])-1+len(df.loc[i,"REF"])
+        
+            if i==df.shape[0]-1:
+                if start_Next>end:
+                    end = start_Next
+                elif start_Next==end:
+                    mut_seq_T2=ref[start_Next]
+                    mut_seq=mut_seq+mut_seq_T2
+                elif start_Next<end:
+                    mut_seq_T2=ref[start_Next:end+1]
+                    mut_seq=mut_seq+mut_seq_T2
+                    mut_seq=mut_seq.upper()
+    
+    records = []
+    rec1 = SeqRecord(Seq(ref[start:end+1].upper(),),id="Ref_seq",description=loc,)
+    rec2 = SeqRecord(Seq(mut_seq),id="Mut_seq",description=line3,)
+    records.append(rec1)
+    records.append(rec2)
+
+    if complement=="1":
+        rec3 = SeqRecord(Seq(mut_seq).complement(),id="Complement_Mut_seq",description="Complement of mutated sequence",)
+        records.append(rec3)        
+    
+    # The actual biological transcription process works from the template strand, doing a reverse complement (TCAG CUGA) to give the mRNA. However, in Biopython and bioinformatics in general, we 
+    # typically work directly with the coding strand because this means we can get the mRNA sequence just by switching T U.
+    if transcript=="1":
+        rec4 = SeqRecord(Seq(mut_seq).transcribe(),id="Transcription_Mut_seq",description="Transcription of coding strand",)
+        records.append(rec4)    
+    
+    if translate=="1":  
+        if table_def!="":
+            Table=int(table_def) 
+        else:
+            Table=1
+        rec5 = SeqRecord(Seq(mut_seq).translate(table=Table),id="Translation_Mut_seq",description="Translation of coding strand",)
+        records.append(rec5)
+    
+    SeqIO.write(records, "Mutation_sequence.fa", "fasta")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
