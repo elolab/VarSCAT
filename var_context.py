1626f12411d42c36de171452971f71decca2d9d2
Ning Wang <ning.wang@utu.fi>
2022-03-19 22:25:58 +0000

    Upload New File

---
+++ var_context.py
@@ -0,0 +1,103 @@
+from Bio.Seq import Seq
+import read_reference
+
+def mutation_seq(df,ref_file,based):
+	REF_seq=[]
+	MUT_seq=[]
+	chr_name=""
+	for i in range(0,df.shape[0]):
+		chromosome=df.loc[i,"Chromosome"]
+		if chr_name!=chromosome:
+			chr_name=chromosome
+			ref=read_reference.read_reference_sequence(ref_file,chr_name)
+		if len(df.loc[i,"REF"]) > len(df.loc[i,"ALT"]):
+			
+			if df.loc[i,"REF"].upper()[0]==df.loc[i,"ALT"].upper()[0]:
+				S = df.loc[i,"5'_aligned"]-1-based
+				E = df.loc[i,"3'_edge"]+1+1-based
+				Ref_seq = Seq(ref[S:E].upper())
+				Mut_seq = Seq(ref[S].upper())+" -"*(len(df.loc[i,"REF"])-1)+Seq(ref[df.loc[i,"5'_aligned"]+len(df.loc[i,"REF"])-1-based:E].upper())			
+				REF_seq.append(str(Ref_seq))
+				MUT_seq.append(str(Mut_seq))
+			elif df.loc[i,"REF"].upper()[0]!=df.loc[i,"ALT"].upper()[0]:
+				S = df.loc[i,"5'_aligned"]-1-based
+				E = df.loc[i,"3'_edge"]+1+1-based
+				Ref_seq = Seq(ref[S:E].upper())
+				Mut_seq = Seq(ref[S].upper())+Seq(df.loc[i,"ALT"][0:].upper())+"-"*(len(df.loc[i,"REF"])-1)+Seq(ref[df.loc[i,"3'_edge"]+len(df.loc[i,"REF"])-1-based:E].upper())			
+				REF_seq.append(str(Ref_seq))
+				MUT_seq.append(str(Mut_seq))
+		elif len(df.loc[i,"REF"]) < len(df.loc[i,"ALT"]):
+			if df.loc[i,"REF"].upper()[0]==df.loc[i,"ALT"].upper()[0]:
+				S = df.loc[i,"5'_aligned"]-based 
+				E = df.loc[i,"3'_edge"]+1+1-based 
+				Ref_seq = Seq(ref[S:E].upper())
+
+				Mut_seq = Seq(df.loc[i,"ALT"].upper())+Seq(ref[S+1:E].upper())				
+				REF_seq.append(str(Ref_seq))
+				MUT_seq.append(str(Mut_seq))
+			elif df.loc[i,"REF"].upper()[0]!=df.loc[i,"ALT"].upper()[0]:
+				S = df.loc[i,"5'_aligned"]-1-based 
+				E = df.loc[i,"3'_edge"]+1+1-based 
+				Ref_seq = Seq(ref[S:E].upper())
+				Mut_seq = Seq(ref[S].upper())+Seq(df.loc[i,"ALT"].upper())+Seq(ref[S+1+1:E].upper())
+				REF_seq.append(str(Ref_seq))
+				MUT_seq.append(str(Mut_seq))
+		elif len(df.loc[i,"REF"]) == len(df.loc[i,"ALT"]): 
+			S = df.loc[i,"5'_aligned"]-1-based
+			E = df.loc[i,"3'_edge"]+1+1-based 
+			Ref_seq = Seq(ref[S:E].upper())
+			Mut_seq = Seq(ref[S].upper())+df.loc[i,"ALT"]+Seq(ref[df.loc[i,"5'_aligned"]+len(df.loc[i,"REF"])-based:E].upper())
+			REF_seq.append(str(Ref_seq))
+			MUT_seq.append(str(Mut_seq))
+	
+	return REF_seq, MUT_seq
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
