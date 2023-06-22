f1ca66b9da32f97314819d58714793899a6a1217
Ning Wang <ning.wang@utu.fi>
2023-06-22 12:31:43 +0000

    Update VarSCAT.py

--- VarSCAT.py
+++ VarSCAT.py
@@ -277,7 +277,7 @@ Output the reference sequence, the mutated sequence and the reverse complement o
 $python VarSCAT.py -A --mut_seq 1 --complement 1 --location chr_test:20-30 --vcf ./data/test.vcf.gz --reference ./data/test.fa --output output_location
 
 Parse variants for several locations in a bed file
-$python VarSCAT.py -A --LRP 1 --HGVS 1 --flank 1 --neighbor 1 --bed ./data/regions.bed --vcf ./data/test.vcf.gz --reference ./data/test.fa --output output_bed
+$python VarSCAT.py -A --LRP 1 --HGVS 1 --flank 1 --neighbor 1 --annotation ./data/custom.bed --bed ./data/regions.bed --vcf ./data/test.vcf.gz --reference ./data/test.fa --output output_bed
 
 Output flanking bases of variants and tandem repeat regions with default setting
 $python VarSCAT.py -A --flank 1 -T --vcf ./data/test.vcf.gz --reference ./data/test.fa --output output_TR
